f7a4e108cfd99a914583c95f02a282594e7e4075
Pankaj Wakchaure <pankaj.corp@gmail.com>
2022-08-09 19:52:23 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 import UseMemo from "./UseMemo";
 import UseState from "./UseState";
+import UseEffect from "./UseEffect";
 
 function App() {
   return (
     <div className="App">
       {/* <UseMemo></UseMemo> */}
-      <UseState></UseState>
+      {/* <UseState></UseState> */}
+      <UseEffect></UseEffect>
     </div>
   );
 }
---
+++ src/UseEffect.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function UseEffect() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const updateWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("useEffect code runs");
+
+    window.addEventListener("resize", updateWindowWidth);
+    return () => {
+      console.log("cleanup code runs");
+      window.removeEventListener("resize", updateWindowWidth);
+    };
+  }, []);
+
+  return (
+    <>
+      <div>Window Width: {windowWidth}</div>
+    </>
+  );
+}
